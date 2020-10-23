efd57571f022a3841bcc8f2d5ed20714d6ad2b6f
Julio <jn5.guape@gmail.com>
2020-10-23 10:50:59 -0300

    att

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ class Bot(discord.Client):
             )
         elif '$' in message.content:
             await message.channel.send(
-                'Comando invalido!'
+                'Comando invalido! Digite $help para receber ajuda'
             )
 
 
